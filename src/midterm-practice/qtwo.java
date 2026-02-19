4a1d6098945a0de618bc38d0a508daef2dcd00ca
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-02-19 14:57:00 -0500

    all question 2

---
+++ src/midterm-practice/qtwo.java
@@ -0,0 +1,133 @@
+/* All question 2
+
+Question 2.1
+
+Write a program that prints the following exactly:
+
+
+
+Welcome to Java Programming!
+
+
+
+Question 2.2
+
+Write a program that prints a rectangle using stars:
+
+
+
+*****
+
+*****
+
+*****
+
+
+
+Question 2.3
+
+Write a program that prints your name, age, and favorite subject on separate lines.
+
+
+
+Question 2.4
+
+Write a program that prints:
+
+
+
+The sum of 10 and 20 is 30
+
+(Use variables.)
+
+
+
+Question 2.5
+
+Write a program that demonstrates the difference between print() and println().
+
+
+*/
+
+
+
+/* Question 2.1
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
+
+
+
+*/
+
+
+
+/* Question 2.2
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+    }
+}
+
+
+
+*/
+
+
+
+/* Question 2.3
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name: Dragon");
+        System.out.println("Age: 21");
+        System.out.println("Favorite Subject: Computer Science");
+    }
+}
+
+
+
+
+*/
+
+
+/* Question 2.4
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
+
+
+
+
+*/
+
+/* Question 2.5
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("This is print.");
+        System.out.print(" This is still on the same line.");
+
+        System.out.println(); // moves to a new line
+
+        System.out.println("This is println.");
+        System.out.println("This is on a new line.");
+    }
+}
+
+
+
+*/
